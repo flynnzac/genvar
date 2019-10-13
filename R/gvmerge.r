94976f8a46bfaf3df7f8bdd237628439e6485b57
Zach Flynn <zlflynn@gmail.com>
2019-10-13 12:23:06 -0400

    Added gvmerge

---
+++ R/gvmerge.r
@@ -0,0 +1,44 @@
+#' merge two datasets
+#'
+#' Merges two datasets using either a left (keep all elements in current datasets and replace with missing if not present in new set), right (keep all elements in new dataset), outer (keep all observations in both datasets), or inner join (only keep elements in both datasets)
+#' @param data dataset to merge in, either an R data frame, a csv file name, or a dta (Stata) file name.
+#' @param on a variable list to merge on with the form "var1 var2 var3".
+#' @param kind one of "left", "right", "outer", or "inner" (default: "left")
+#' @param ... extra options to pass to \code{read.csv} or \code{read.dta} (for old Stata files) or \code{read.dta13} (for newer ones).
+#' @return returns NULL, invisibly
+#' @export
+gvmerge <- function (data, on, kind="left", ...)
+{
+  UseMethod("gvmerge", data)
+}
+
+gvmerge.data.frame <- function (data, on, kind="left", ...)
+{
+
+  m <- merge(get("data", envir=data.env), data,
+             by=strsplit(on, "\\s+", perl=TRUE)[[1]],
+             all.x=(kind=="left" | kind=="outer"),
+             all.y=(kind=="right" | kind=="outer"))
+
+  use(m, clear=TRUE)
+  invisible(NULL)
+}
+
+gvmerge.character <- function (data, on, kind="left")
+{
+  type <- file_ext(data)
+
+  if (type=="csv")
+  {
+    data <- read.csv(data, ...)
+  } else if (type=="dta") {
+    data <- tryCatch({ read.dta(data, ...) },
+                     error=function (e)
+                       read.dta13(data, ...))
+  } else {
+    stop("File type must be either CSV or DTA (Stata).")
+  }
+
+  gvmerge(data, on, kind)
+}
+      
