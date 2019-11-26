a8302c765396c0878febec75896f6ad3e5d15fbd
Zach Flynn <zlflynn@gmail.com>
2019-11-26 13:52:04 -0500

    Documentation update

--- R/switchdata.r
+++ R/switchdata.r
@@ -3,6 +3,18 @@
 #' @param name Name of the dataset to switch to.  The name "last" is reserved for the last dataset loaded (default: last)
 #' @param clear If TRUE, erase the currently loaded dataset.  If FALSE, make the current dataset the new "last". (default: FALSE)
 #' @return returns NULL invisibly
+#' @examples
+#' use(cars, clear=TRUE)
+#' namedata("cars", clear=TRUE)
+#' library(plm)
+#' data(Produc)
+#' use(Produc, clear=TRUE)
+#' listif()
+#' switchdata("cars")
+#' listif()
+#' switchdata()
+#' listif()
+#' listdata()
 #' @export
 switchdata <- function (name="last", clear=FALSE)
 {
@@ -21,6 +33,16 @@ switchdata <- function (name="last", clear=FALSE)
 #' @param original a string giving the current name of the dataset or NULL to set the name for the currently-loaded dataset (default: NULL)
 #' @param clear if TRUE, overwrite current dataset at that name, if FALSE, stop if the dataset already exists (default: FALSE).
 #' @return returns NULL invisibly
+#' @examples
+#' use(cars, clear=TRUE)
+#' namedata("cars", clear=TRUE)
+#' library(plm)
+#' data(Produc)
+#' use(Produc, clear=TRUE)
+#' namedata("product", clear=TRUE)
+#' listdata()
+#' namedata("cars_new_name", original="cars", clear=TRUE)
+#' listdata()
 #' @export
 namedata <- function (name, original=NULL, clear=FALSE)
 {
@@ -41,6 +63,16 @@ namedata <- function (name, original=NULL, clear=FALSE)
 #' lists datasets currently in memory
 #'
 #' @return returns a data frame of the names of the datasets in memory and basic descriptions (number of observations, variables).
+#' @examples
+#' use(cars, clear=TRUE)
+#' namedata("cars", clear=TRUE)
+#' library(plm)
+#' data(Produc)
+#' use(Produc, clear=TRUE)
+#' namedata("product", clear=TRUE)
+#' listdata()
+#' namedata("cars_new_name", original="cars", clear=TRUE)
+#' listdata()
 #' @export
 listdata <- function ()
 {
